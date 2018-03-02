0c5fbf1d6222e40665d87e8b8de2739469650448
Joseph Karanja <Karanja@jubileekenya.com>
2018-03-02 08:18:54 +0300

    Enabled cors

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
