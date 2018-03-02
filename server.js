7fec6907762ee0224a99562b4445cb5a581d52a9
Joseph Karanja <Karanja@jubileekenya.com>
2018-03-02 08:13:28 +0300

    added imports in server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
-    morgan  = require('morgan');
+    morgan  = require('morgan'),
+    passport = require('passport'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
     
 Object.assign=require('object-assign')
 
